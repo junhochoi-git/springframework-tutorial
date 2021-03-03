727502adf23f386dcae9c12899c7d5bfd26f8407
junhochoi-git <ajchoi0928@gmail.com>
2021-03-03 23:51:51 +0900

    Handling CRUD, p383

--- ex04_0220/src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ ex04_0220/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -1,10 +1,13 @@
 package org.zerock.mapper;
 
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.ReplyVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -26,7 +29,17 @@ public class ReplyMapperTests {
 	}
 	
 	@Test 
-	public void testInsert() {
+	public void testCreate() {
+		IntStream.rangeClosed(1, 10).forEach(i->{
+			ReplyVO vo = new ReplyVO();
+			
+			vo.setBno(bnoArr[i%5]);
+			vo.setReply("댓글 테스트" + i);
+			vo.setReplyer("replyer"+i);
+			
+			mapper.insert(vo);
+			
+		});
 		
 	}
 }
