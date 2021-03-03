482ce6257a22ee56a6ae06d07bbae947cee7ab8b
junhochoi-git <ajchoi0928@gmail.com>
2021-03-04 00:18:45 +0900

    update Create Read Update Delete SQL and JUnit Tests.

--- ex04_0220/src/main/java/org/zerock/mapper/ReplyMapper.java
+++ ex04_0220/src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -6,4 +6,12 @@ public interface ReplyMapper {
 	
 	public int insert(ReplyVO vo); // use foreign key bet. pk tbl_board(bno) and tbl_reply(bno)
 	
+	/*p384 Read*/
+	public ReplyVO read(Long bno); 
+	
+	/*p385 Delete */
+	public int delete(Long bno);
+	
+	/*p386 Update*/
+	public int update(ReplyVO reply);
 }
--- ex04_0220/src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ ex04_0220/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -17,29 +17,54 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class ReplyMapperTests {
 	
-	/*테스트 전 해당 번호의 게시물이 존재하는지 반드시 확인.*/
-	private Long[] bnoArr = {1310764L, 1310763L, 1310762L, 1310741L, 1310725L};
-	
-	@Setter(onMethod_=@Autowired)
+	/* 테스트 전 해당 번호의 게시물이 존재하는지 반드시 확인. */
+	private Long[] bnoArr = { 1310764L, 1310763L, 1310762L, 1310741L, 1310725L };
+
+	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
-	
+
 	@Test
-	public void testMapper()	{
+	public void testMapper() {
 		log.info(mapper); /* Test whether it is available to use an object of ReplyMapper */
 	}
-	
-	@Test 
+
+	@Test
 	public void testCreate() {
-		IntStream.rangeClosed(1, 10).forEach(i->{
+		IntStream.rangeClosed(1, 10).forEach(i -> {
 			ReplyVO vo = new ReplyVO();
-			
-			vo.setBno(bnoArr[i%5]);
+
+			vo.setBno(bnoArr[i % 5]);
 			vo.setReply("댓글 테스트" + i);
-			vo.setReplyer("replyer"+i);
-			
+			vo.setReplyer("replyer" + i);
+
 			mapper.insert(vo);
-			
+
 		});
+	}
+
+	@Test
+	public void testRead() {
+		Long targetBno = 5L; // Bno =5 인걸 Read() 할래
+		ReplyVO vo = mapper.read(targetBno);
+		log.info(vo);
+	}
+
+	@Test
+	public void testDelete() {
+		Long targetRno = 1L; // Rno = 1 인걸
+		mapper.delete(targetRno);
+	}
+
+	@Test 
+	public void testUpdate() {
+		Long targetRno = 10L;
 		
+		ReplyVO vo = mapper.read(targetRno); // update object ReplyVO referenced as vo
+		
+		vo.setReply("Update Reply");
+		
+		int count = mapper.update(vo);
+		
+		log.info("UPDATE COUNT: "+ count );
 	}
 }
